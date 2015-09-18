ad5c1dc0a8465d4dac4fcefbfb894c9ef02ae5f1
Patrick Daly <patrickrorydaly@gmail.com>
2015-09-17 20:12:19 -0700

    upload radius SOF

---
+++ routes.py
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# route class should compare drivers with passengers classes to determine closest radius
+# should dispatch driver to passengers
+
+import cars
+
+class route(object):
+    def __init__(self):
+        self.id = 0
+        #latitude longtitude pair
+        self.pair = [driver, passenger]
+        radius = ['NW']
+        driverOnline = 1
+        passengerOnline
+        orderStatus = 0.25
+
+# define setter and getter for remote_addr_w_port
+    def getRadius(self):
+        print self.radius
+        return self.radius
+
+    def setRadius(self):
+        print "enter a value for the remote address and port"
+        tempRadius = raw_input()
+        self.radius = int(tempRadius)
