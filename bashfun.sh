40e0dce0f4b06ade852ace0ac6ba82ffe08a7629
Patrick Daly <patrickrorydaly@gmail.com>
2015-09-20 20:32:51 -0700

    add case

--- bashfun.sh
+++ bashfun.sh
@@ -9,3 +9,31 @@ function first_function {
 }
 
 first_function
+
+function second_function {
+  echo "this is the second function"
+
+}
+
+function input_function {
+  echo "the name you gave me is: $1"
+}
+
+input_function patrick
+
+input_function $input
+
+case $input in
+    1)
+      first_function
+    ;;
+    2)
+      second_function
+    ;;
+    3)
+      input_function $input2
+    ;;
+    *)
+      echo "You didn't select a valid option"
+    ;;
+esac
