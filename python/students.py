8897ee99ba1142d2e933255b4539f61a81c5cf91
Patrick Daly <patrickrorydaly@gmail.com>
2015-09-22 06:32:54 -0700

    update parameters

--- python/students.py
+++ python/students.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 class Student(object):
-    def __init__(self):
-        self.name = "None"
-        self.grade = "K"
-        self.council = "Killara"
+    def __init__(self, name ="None", grade = "K", council = "Killara"):
+        self.name = name
+        self.grade = grade
+        self.council = council
 
 student1 = Student()
 print student1.name
+student2 = Student(name = "Byron", grade = "12", council="Willoughby")
+print student2.name
