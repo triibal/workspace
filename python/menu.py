a632073884676ed34b42cf53e5dbccfdfb9e374e
Patrick Daly <patrickrorydaly@gmail.com>
2015-09-22 08:45:54 -0700

    add docustring

---
+++ python/menu.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# create class for MenuItem
+
+class MenuItem(object):
+    """docstring for MenuItem"""
+    def __init__(self, arg):
+        super(, self).__init__()
+        self.arg = arg
