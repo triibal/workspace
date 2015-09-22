989e0b647a9afbd9af3e8753a2f9ac10916be8c5
Patrick Daly <patrickrorydaly@gmail.com>
2015-09-22 06:28:02 -0700

    add students stub

---
+++ python/students.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+class Student(object):
+    def __init__(self):
+        self.name = "None"
+        self.grade = "K"
+        self.council = "Killara"
+
+student1 = Student()
+print student1.name
