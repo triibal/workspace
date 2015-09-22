3f0e9720834b198c7d6373456ca240b1a12d2ac4
Patrick Daly <patrickrorydaly@gmail.com>
2015-09-22 06:52:17 -0700

    fix indentation and add def

--- python/students.py
+++ python/students.py
@@ -17,27 +17,34 @@ class Student(object):
 
 # create function to input and get grade value to student object
 
-def get_grade(self):
-    while True:
-        grade = raw_input("What is the student's grade? [K, 1 - 12] ")
-        if grade.lower() not in ['k','1','2','3','4','5','6','7','8','9','10','11','12']
-            print " {} isn't valid".format(grade)
-        else:
-            return grade
+    def get_grade(self):
+        while True:
+            grade = raw_input("What is the student's grade? [K, 1 - 12] ")
+            if grade.lower() not in ['k','1','2','3','4','5','6','7','8','9','10','11','12']
+                print " {} isn't valid".format(grade)
+            else:
+                return grade
 
 # function that prints student object values
 
-def print_student(self):
-    print "Name: {}".format(self.name)
-    print "School: {}".format(self.school)
-    print "Grade: {}".format(self.grade)
+    def print_student(self):
+        print "Name: {}".format(self.name)
+        print "School: {}".format(self.school)
+        print "Grade: {}".format(self.grade)
+
+
+def print_roster(students):
+    print "Students in the program:"
+    for student in students:
+        print "8"*15
+        student.print_student()
 
 def main():
-    student1 = Student()
+    student1 = Student(name = "David", school = "Cranbrook", grade = "11")
     student2 = Student(name = "Byron", school = "Willoughby", grade = "12")
-    student1.print_student
-    student2.print_student
-
+    student3 = Student(name = "Nick", school ="North Sydney", grade = "11")
+    students = [student1, student2, student3]
+    print_roster(students)
 # call main function, will create student1 and student2 and print info
 
 if __name__ == "__main__"
